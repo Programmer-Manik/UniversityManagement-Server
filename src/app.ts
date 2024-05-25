7c0c737850be127575bf88cc02a8607d1b2abb35
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-25 07:03:29 +0600

    Create Not Found Route & SendResponse Utility

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import express, { Application,  Request, Response,  } from 'express';
 import { StudentRoutes } from './app/modules/student/student.route';
 import { UserRoutes } from './app/modules/user/user.route';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
@@ -22,5 +24,9 @@ const getAController = (req: Request, res: Response) => {
 
 app.get('/', getAController);
 
+app.use(globalErrorHandler)
+
+// not found routes
+app.use(notFound);
 
 export default app;
