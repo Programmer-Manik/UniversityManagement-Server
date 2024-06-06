a200a7bf67272f70c53568eab24f6f4b5f5f52a3
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-06 12:38:13 +0600

    Understanding Error Patterns In Zod And Mongoose

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,20 +2,55 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+// const globalErrorHandler = (
+//   err: any,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction,
+// ) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || 'Something went wrong!';
+
+//   return res.status(statusCode).json({
+//     success: false,
+//     message,
+//     error: err,
+//   });
+
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong!';
+  //settings default error
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+  type TErrorSource = {
+    path:string | number;
+    message:string;
+  }[];
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+
+  if(err instanceof ZodError){
+    statusCode = 400;
+    message =  'Something went wrong!';
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSource,
     error: err,
   });
 };
