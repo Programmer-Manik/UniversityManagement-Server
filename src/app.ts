62d11c867359c64737cbe52028671518528744c3
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-06 16:14:06 +0600

    How To Handle Error, AppError, UnhandledRejection, UncaughtException

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 
 import cors from 'cors';
-import express, { Application,} from 'express';
+import express, { Application, Request, Response,} from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
@@ -10,6 +12,16 @@ const app: Application = express();
 //parsers
 app.use(express.json());
 app.use(cors());
+
+
+//testing
+const test = async (_req:Request, res:Response) => {
+    const a = 10 ;
+    res.send(a);
+};
+app.get('/test', test);
+
+
 
 // application routes
 app.use('/api/v1/', router);
