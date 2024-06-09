992446ee9921cd1bb3912a22e6a5aa20d2618db0
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-10 01:28:13 +0600

    Refactor Your Code And Build A Query Builder

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -154,26 +154,26 @@ const studentSchema = new Schema<TStudent, StudentModel>(
   },
 );
 
-// // virtual
-// studentSchema.virtual('fullName').get(function () {
-//   return this.name.firstName + this.name.middleName + this.name.lastName;
-// });
+// virtual
+studentSchema.virtual('fullName').get(function () {
+  return this?.name?.firstName + this?.name?.middleName + this?.name?.lastName;
+});
 
-// // Query Middleware
-// studentSchema.pre('find', function (next) {
-//   this.find({ isDeleted: { $ne: true } });
-//   next();
-// });
+// Query Middleware
+studentSchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
 
-// studentSchema.pre('findOne', function (next) {
-//   this.find({ isDeleted: { $ne: true } });
-//   next();
-// });
+studentSchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
 
-// studentSchema.pre('aggregate', function (next) {
-//   this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
-//   next();
-// });
+studentSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
 
 //creating a custom static method
 studentSchema.statics.isUserExists = async function (id: string) {
