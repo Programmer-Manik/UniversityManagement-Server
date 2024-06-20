90233b825765443a55f176424e5a478b46ec4c20
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-21 02:02:34 +0600

    password reset doen and forgot password done

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -44,7 +44,15 @@ const createAdmin = catchAsync(async (req, res) => {
 
 
 const getMe = catchAsync(async (req, res) => {
-  const result = await UserServices.getMeDB(req.user);
+  // const token = req.headers.authorization;
+
+  // if (!token) {
+  //   throw new AppError(httpStatus.NOT_FOUND, 'Token not found !');
+  // }
+
+  const { userId, role } = req.user;
+
+  const result = await UserServices.getMeDB(userId, role);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -54,9 +62,23 @@ const getMe = catchAsync(async (req, res) => {
   });
 });
 
+const changeStatus = catchAsync(async (req, res) => {
+  const id = req.params.id;
+
+  const result = await UserServices.changeStatus(id, req.body);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Status is updated succesfully',
+    data: result,
+  });
+});
+
 export const UserControllers = {
   createStudent,
   createFaculty,
   createAdmin,
-  getMe
+  getMe,
+  changeStatus,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -200,12 +200,19 @@ const getMeDB = async (userId: string, role: string) => {
   return result;
 };
 
+const changeStatus = async (id: string, payload: { status: string }) => {
+  const result = await User.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
+  return result;
+};
 
 export const UserServices = {
   createStudentIntoDB,
   createFacultyIntoDB,
   createAdminIntoDB,
-  getMeDB
+  getMeDB,
+  changeStatus,
 };
 
 
