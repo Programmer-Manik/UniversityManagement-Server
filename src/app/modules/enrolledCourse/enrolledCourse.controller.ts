af7abbb98b2d9f866914dd98f3bfb6168a42436d
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-29 15:16:35 +0600

    Dynamically Update Course Marks For Specific Faculty

--- src/app/modules/enrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.controller.ts
@@ -1,42 +1,39 @@
-import httpStatus from "http-status";
-import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
-import { EnrolledCourseServices } from "./enrolledCourse.service";
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { EnrolledCourseServices } from './enrolledCourse.service';
 
+const createEnrolledCourse = catchAsync(async (req, res) => {
+  const userId = req.user.userId;
+  const result = await EnrolledCourseServices.createEnrolledCourseIntoDB(
+    userId,
+    req.body,
+  );
 
-const createEnrolledCourse = catchAsync(async (req, res)=>{
-   
-   const userId = req.user.userId;
-    const result = await EnrolledCourseServices.createEnrolledCourseIntoDB(userId, req.body);
-
-//    console.log(req.user, "user")
-
-    sendResponse(res,{
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Enrolled Course Created Successfully",
-        data: result,
-    })
-})
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is enrolled succesfully',
+    data: result,
+  });
+});
 
 const updateEnrolledCourseMarks = catchAsync(async (req, res) => {
-    // console.log(req.user, "user")
-    const facultyId = req.user.userId;
-    const result = await EnrolledCourseServices.updateEnrolledCourseMarksIntoDB(
-     facultyId,
-      req.body,
-    );
-  
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Marks is updated succesfully',
-      data: '',
-    });
-  });
+  const facultyId = req.user.userId;
+  const result = await EnrolledCourseServices.updateEnrolledCourseMarksIntoDB(
+    facultyId,
+    req.body,
+  );
 
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Marks is updated succesfully',
+    data: result,
+  });
+});
 
 export const EnrolledCourseControllers = {
-    createEnrolledCourse,
-    updateEnrolledCourseMarks
-}
+  createEnrolledCourse,
+  updateEnrolledCourseMarks,
+};
